212067b37436e4ebff7e51b0fe04b12e406f66ce
Charity <umercy001@gmail.com>
2022-09-12 13:29:28 +0100

    simple Profile

---
+++ index.js
@@ -0,0 +1,8 @@
+titleEl = document.getElementById("title-el")
+skillEl = document.getElementById("skill-el")
+impEl = document.getElementById("imp-el")
+contactEl = document.getElementById("contact")
+
+
+titleEl.innerHTML = "My Developer's Profile"
+// contactEl.innerHTML = `${}`
